a9dbda53498bbe28540babe1f9194b1a0fdd5409
robert-hh <robert@hammelrath.com>
2023-05-09 07:44:26 +0200

    mimxrt: First attempt to enable _thread.
    Compiles, starts and runs a thread. To be tested further
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/mimxrt/mphalport.h
+++ ports/mimxrt/mphalport.h
@@ -83,6 +83,7 @@ extern ringbuf_t stdin_ringbuf;
 #define mp_hal_quiet_timing_exit(irq_state) restore_irq_pri(irq_state)
 
 void mp_hal_set_interrupt_char(int c);
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
 
 static inline mp_uint_t mp_hal_ticks_ms(void) {
     return ticks_ms32();
